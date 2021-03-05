89f43e784f8630d25bb576c6f756c2c1a397e5b5
Iskren Kostov <63412914+voidler@users.noreply.github.com>
2021-03-05 12:27:54 +0200

    Add files via upload

--- CourseProject2021/src/AvatarPosition.java
+++ CourseProject2021/src/AvatarPosition.java
@@ -1,2 +1,7 @@
-public class AvatarPosition {
+public class AvatarPosition extends Scheme {
+    String type ;
+    public AvatarPosition(int startX, int start_y, int endX, int endY, BaseModel baseModel, String typeIn) {
+        super(startX, start_y, endX, endY, baseModel);
+        type =typeIn;
+    }
 }
