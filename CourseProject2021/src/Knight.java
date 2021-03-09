b49fb2d3bd15b1060d4220e82ca5fb4c8447d6de
Iskren Kostov <63412914+voidler@users.noreply.github.com>
2021-03-09 14:38:41 +0200

    Add files via upload

--- CourseProject2021/src/Knight.java
+++ CourseProject2021/src/Knight.java
@@ -1,2 +1,16 @@
-public class Knight {
+import java.awt.*;
+
+public class Knight extends BaseModel {
+    public Knight(Color color) {
+        super(color);
+    }
+    public Knight(int team)
+    {
+        super(team);
+        attack = 8;
+        armor = 3;
+        health = 15;
+        attack_square = 1;
+        speed = 1;
+    }
 }
--- CourseProject2021/src/LittleMan.java
+++ CourseProject2021/src/LittleMan.java
@@ -1,2 +1,17 @@
-public class LittleMan {
+import java.awt.*;
+
+public class LittleMan extends  BaseModel{
+    public LittleMan(Color color) {
+        super(color);
+    }
+    public LittleMan(int team)
+    {
+        super(team);
+        attack = 6;
+        armor = 2;
+        health = 12;
+        attack_square = 2;
+        speed = 2;
+    }
+
 }
